b16b4866519bfff0960215fc8600f36eae0d6576
CyberMavka <maksym.pavlenko.cm@gmail.com>
2020-12-02 23:46:54 +0200

    new

--- Library_only_da/main.cpp
+++ Library_only_da/main.cpp
@@ -1,4 +1,33 @@
 #include <iostream>
+struct Day{
+    int day;
+};
+
+struct Month{
+    int month;
+};
+
+struct Year{
+    int year;
+};
+
+int getDayIndex(int days, int month, int year){
+    int daysOfTheMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    for (int i = 0; i < month - 1; i++)
+    {
+        days += daysOfTheMonth[i];
+    }
+    if ( year%4==0 and year%100==0 and year%400==0){
+        days += 1;
+    }
+    return days;
+}
+
+struct Date{
+    int day;
+    int month;
+    int year;
+};
 
 int main() {
     return 0;
